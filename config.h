a17606952d375d9739afd4dd7927142e87fe95e7
germes <1germes@gmail.com>
2019-06-10 21:18:10 +0300

    kill deatached tmux sessions

--- config.h
+++ config.h
@@ -60,7 +60,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", "tmux", "new" };
+static const char *termcmd[]  = { "/home/germes/code/dotfiles/scripts/start_terminal.sh" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
